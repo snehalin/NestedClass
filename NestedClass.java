d69ebb0fa3bb048f4956901adf4cc15cd5b08741
snehalin <55582815+snehalin@users.noreply.github.com>
2020-09-19 13:07:08 +0530

    Add files via upload

---
+++ NestedClass.java
@@ -0,0 +1,52 @@
+/*
+ Nested class
+ 1.MemberInnerClass--->Outside method
+ 2.LocalInnerClass---->Inside method
+ 3.StaticInnerClass--->outside method and static 
+ 4.AnnonymousInnerClass-->outside/inside method
+  
+  
+  class OuterClass
+ {
+  void m()
+  {
+  
+  }
+ 
+ 
+ }
+ */
+public class NestedClass {
+	int a;
+	NestedClass()
+	{
+		a=10;
+	}
+	void m()
+	{
+		 System.out.println("in method a="+a+" ");
+	}
+	 
+	class InnerClass//MemberInner
+	{
+	  int b;
+	  InnerClass()
+	  {
+		  b=20;
+	  }
+	  void m2()
+	  {
+		  System.out.println("in method m2 a="+a+"b="+b);  
+	  }
+	
+	}
+	
+	public static void main(String args[])
+	{
+		NestedClass ob=new NestedClass();
+		NestedClass.InnerClass  ob2=ob.new InnerClass();
+		ob2.m2();
+		ob.m();
+	}
+	
+}
